406f2ae5b5b6b91d9fa1114dc456a7d41923c271
KtourzaJeremy <jeremyktourza@gmail.com>
2024-01-16 23:27:03 +0100

    Composant Home et Binge mise en forme Mise en place de l'UI principale pour les pages Home qui servira d'acceuil et de formulaire d'insertion de films et Binge qui affichera les résulats Routing fonctionnel Mode sombre par défaut Menu déroulants pour accéder aux cinémas acceptant les cartes illimtés UGC ou Cinépass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BingeComponent } from './binge/binge.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, HomeComponent, BingeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BingeComponent } from './binge/binge.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home page',
+      },
+      {
+        path: 'binge',
+        component: BingeComponent,
+        title: 'Results',
+      },
+];
